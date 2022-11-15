7974084d357a8fc08a50d46f8b2d470a674cfe83
dopewevmond <dopewevmond@gmail.com>
2022-11-15 21:28:41 +0000

    log request middleware

--- src/middleware/middleware.logrequest.ts
+++ src/middleware/middleware.logrequest.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 
+/**
+ * Logs the method, url and date/time of a request to the console
+ * @param req Request object
+ * @param res Response object
+ * @param next Call next middleware
+ */
 const logRequestDetails = (req: Request, res: Response, next: NextFunction): void => {
-  console.log(`${req.method} ${req.url} ${new Date().getDate()}`)
+  const dateTime = new Date()
+  console.log(`${req.method} ${req.url} --- ${dateTime.toDateString()} --- ${dateTime.toTimeString()}`)
+  next()
 }
 
 export default logRequestDetails
