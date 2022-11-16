38c5cec4f1db888a37572a82ba0ceb5c5e560e80
dopewevmond <dopewevmond@gmail.com>
2022-11-16 11:18:51 +0000

    resources for main controller

--- src/controllers/controller.main.ts
+++ src/controllers/controller.main.ts
@@ -1,5 +1,33 @@
-import Controller from './controller.interface'
 import { Router, Request, Response } from 'express'
+import Controller from './controller.interface'
+import authenticateJWT from '../middleware/middleware.authenticatejwt'
+
+const books = [
+  {
+    author: 'Chinua Achebe',
+    country: 'Nigeria',
+    language: 'English',
+    pages: 209,
+    title: 'Things Fall Apart',
+    year: 1958
+  },
+  {
+    author: 'Hans Christian Andersen',
+    country: 'Denmark',
+    language: 'Danish',
+    pages: 784,
+    title: 'Fairy tales',
+    year: 1836
+  },
+  {
+    author: 'Dante Alighieri',
+    country: 'Italy',
+    language: 'Italian',
+    pages: 928,
+    title: 'The Divine Comedy',
+    year: 1315
+  }
+]
 
 class MainController implements Controller {
   public path = '/'
@@ -10,15 +38,15 @@ class MainController implements Controller {
   }
 
   private setupRoutes (): void {
-    this.router.get('/', this.Homehandler)
+    this.router.get(
+      this.path,
+      authenticateJWT,
+      this.Homehandler
+    )
   }
 
   private Homehandler (req: Request, res: Response): void {
-    res.json(
-      {
-        status: 'success'
-      }
-    )
+    res.json({ books })
   }
 }
 
--- src/middleware/middleware.logrequest.ts
+++ src/middleware/middleware.logrequest.ts
@@ -2,9 +2,6 @@ import { Request, Response, NextFunction } from 'express'
 
 /**
  * Logs the method, url and date/time of a request to the console
- * @param req Request object
- * @param res Response object
- * @param next Call next middleware
  */
 const logRequestDetails = (req: Request, res: Response, next: NextFunction): void => {
   const dateTime = new Date()
