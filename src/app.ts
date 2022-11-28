9a31aedc255058e0b780f4963f7a6067227b3b9f
dopewevmond <dopewevmond@gmail.com>
2022-11-28 23:37:33 +0000

    sentry

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import * as express from 'express'
 import Controller from './controllers/controller.interface'
 import * as bodyParser from 'body-parser'
 import * as dotenv from 'dotenv'
-import logRequestDetails from './middleware/middleware.logrequest'
+import * as Sentry from '@sentry/node'
+import '@sentry/tracing'
+import * as morgan from 'morgan'
 
 dotenv.config()
 
@@ -11,16 +13,26 @@ class App {
 
   constructor (controllers: Controller[]) {
     this.app = express()
+    Sentry.init({ dsn: 'https://b76619ae6aa641258556f6cc028ef7a4@o4504203419648000.ingest.sentry.io/4504203423711232' })
+    this.app.use(Sentry.Handlers.requestHandler())
+
     this.setupApplication()
     controllers.forEach(controller => {
       this.app.use('/', controller.router)
     })
+
+    this.app.use(Sentry.Handlers.errorHandler())
+    this.app.use(this.ErrorHandlerMiddleware)
   }
 
   private setupApplication (): void {
     this.app.use(bodyParser.urlencoded({ extended: true }))
     this.app.use(bodyParser.json()) // get json data from request into req.body
-    this.app.use(logRequestDetails)
+    this.app.use(morgan('dev'))
+  }
+
+  private ErrorHandlerMiddleware (error: Error, req: express.Request, res: express.Response, next: express.NextFunction): void {
+    res.status(500).json({ message: error.message })
   }
 
   public listen (): void {
