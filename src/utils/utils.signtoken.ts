6574b6e44c630ec05db6e02ba2f21d597384a99b
dopewevmond <dopewevmond@gmail.com>
2022-11-30 14:31:53 +0000

    loosely coupled reset token expiry time

--- src/utils/utils.signtoken.ts
+++ src/utils/utils.signtoken.ts
@@ -4,6 +4,7 @@ const SECRET = process.env.SECRET as jwt.Secret
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET as jwt.Secret
 const ACCESS_TOKEN_EXPIRY_TIME = process.env.ACCESS_TOKEN_EXPIRY_TIME as string
 const REFRESH_TOKEN_EXPIRY_TIME = process.env.REFRESH_TOKEN_EXPIRY_TIME as string
+const RESET_TOKEN_EXPIRY_TIME = process.env.PASSWORD_RESET_TOKEN_EXPIRY_TIME as string
 
 const signAccessToken = (email: string, role: string, tokenId: string): string => {
   return jwt.sign({ email, role, token_id: tokenId }, SECRET, { expiresIn: parseInt(ACCESS_TOKEN_EXPIRY_TIME) })
@@ -14,7 +15,7 @@ const signRefreshToken = (email: string, role: string, tokenId: string): string 
 }
 
 const signPasswordResetToken = (email: string, tokenId: string): string => {
-  return jwt.sign({ email, token_id: tokenId }, SECRET, { expiresIn: parseInt(REFRESH_TOKEN_EXPIRY_TIME) })
+  return jwt.sign({ email, token_id: tokenId }, SECRET, { expiresIn: parseInt(RESET_TOKEN_EXPIRY_TIME) })
 }
 
 export { signAccessToken, signRefreshToken, signPasswordResetToken }
